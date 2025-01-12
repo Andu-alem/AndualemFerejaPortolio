effaa1658a4488adb4bb4f0753d91e1802f77121
Andualem <andualem.fereja12@gmail.com>
2025-01-12 22:55:20 +0300

    typo fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
             <h3 class="text-2xl text-yellow-400">Hello, I'am Andualem</h3>
           </div>
           <p class="text-center text-[16px] tracking-wide text-white my-3">
-            A seasoned software developer with a Computer Engineering backround and proficiency in fullstack web developement using modern web development tools and standards. Whether you need a well-rounded full-stack developer in your team or want a high quality and modern landing page, blog, portfolio, or an e-commerce website feel free to reach me using your choice of provided addresses.
+            A seasoned software developer with a Computer Engineering background and proficiency in fullstack web developement using modern web development tools and standards. Whether you need a well-rounded full-stack developer in your team or want a high quality and modern landing page, blog, portfolio, or an e-commerce website feel free to reach me using your choice of provided addresses.
           </p>
         </div>
         <div class="relative w-10/12 mx-auto sm:w-1/2 flex flex-col justify-center items-center after:w-[47%] sm:after:w-[77%] after:h-[28vh] sm:after:h-[75%] md:after:h-[80%] md:after:w-[77%] lg:after:h-[85%] lg:after:w-[52%] after:rounded-full after:absolute after:-z-50 after:p-3 after:bg-gradient-to-r after:from-sky-500 after:to-amber-500 after:animate-spin-slow">
