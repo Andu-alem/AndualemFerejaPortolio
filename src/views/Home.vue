e9152c874b8278589557f17406a6d9308b8333e4
Andualem <andualem.fereja12@gmail.com>
2025-01-12 23:20:24 +0300

    animation updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,8 +14,8 @@
             A seasoned software developer with a Computer Engineering background and proficiency in fullstack web developement using modern web development tools and standards. Whether you need a well-rounded full-stack developer in your team or want a high quality and modern landing page, blog, portfolio, or an e-commerce website feel free to reach me using your choice of provided addresses.
           </p>
         </div>
-        <div class="relative w-10/12 mx-auto sm:w-1/2 flex flex-col justify-center items-center after:w-[47%] sm:after:w-[77%] after:h-[28vh] sm:after:h-[75%] md:after:h-[80%] md:after:w-[77%] lg:after:h-[85%] lg:after:w-[52%] after:rounded-full after:absolute after:-z-50 after:p-3 after:bg-gradient-to-r after:from-sky-500 after:to-amber-500 after:animate-spin-slow">
-          <div class="w-[45%] sm:w-[77%] h-[28vh] sm:h-[75%] md:h-[80%] md:w-[75%] lg:h-[85%] lg:w-[50%] rounded-full">
+        <div class="relative w-10/12 mx-auto sm:w-1/2 flex flex-col justify-center items-center after:w-[155px] after:h-[155px] sm:after:w-[235px] sm:after:h-[235px] md:after:h-[277px] md:after:w-[277px] lg:after:h-[307px] lg:after:w-[307px] after:rounded-full after:absolute after:-z-50 after:p-3 after:bg-gradient-to-r after:from-sky-500 after:via-zinc-300 after:to-amber-500 after:animate-spin-slow">
+          <div class="w-[150px] h-[150px] sm:w-[230px] sm:h-[230px] md:w-[270px] md:h-[270px] md:w-[75%] lg:h-[300px] lg:w-[300px] rounded-full">
             <img class="rounded-full w-full h-full" src="../assets/img/andi_profile.jpg" alt="">
           </div>
         </div>
